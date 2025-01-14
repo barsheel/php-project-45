7fca4131a7997e58db6dd02a23cded2d4a8ca745
barsheel <barsheel@gmail.com>
2025-01-14 23:28:11 +0700

    remove function declaration from brain-even
    -remove all function declaration from "brain-even" , now it is in "Even.php" and "Cli.php"
    
    -"brain-even" is just execution file now

--- src/Cli.php
+++ src/Cli.php
@@ -9,13 +9,38 @@ namespace BrainGames\Cli;
 use function cli\line;
 use function cli\prompt;
 
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
 /**
  * Greetings function
+ * @return string $name of the user
  */
-function sayHello(): string
+function askName(): string
 {
     line("Welcome to the Brain Games!\n");
     $name = prompt("May I have your name?");
     line("Hello, %s!", $name);
     return $name;
+}
+
+function printWrongAnswerMessage(string $correctAnswer, string $wrongAnswer): void
+{
+    line("'{$wrongAnswer}' is wrong answer ;(. 
+        Correct answer was '{$correctAnswer}'.");
+}
+
+function printLoseMessage(string $name): void
+{
+    line("Let's try again, {$name}");
+}
+
+function printCorrectAnswerMessage(): void
+{
+    line("Correct!");
+}
+
+function printWinMessage(string $name): void
+{
+    line("Congratulations, {$name}!");
 }
