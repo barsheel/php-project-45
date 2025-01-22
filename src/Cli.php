d404e1b8a00339ccbe08ae6b531d9bb1a0ecf8e2
barsheel <barsheel@gmail.com>
2025-01-22 21:44:28 +0700

    add Cli.php
    - add Cli.php and askName() function in brain-games again for passing test

---
+++ src/Cli.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Cli functions
+ */
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+/**
+ * Greetings function
+ */
+function askName(): void
+{
+    line("Welcome to the Brain Games!\n");
+    $name = prompt("May I have your name?");
+    line("Hello, %s!", $name);
+}
